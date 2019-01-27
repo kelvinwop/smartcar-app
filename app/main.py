7525394da501fd5f022c5484ed6cfdd242cf8705
Edgariphdz98 <47074756+Edgariphdz98@users.noreply.github.com>
2019-01-27 03:11:09 -0600

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,14 +1,7 @@
 import smartcar
 from flask import Flask, redirect, request, jsonify, render_template
 from flask_cors import CORS
-
 import os
-
-app = Flask(__name__)
-CORS(app)
-
-# global variable to save our access_token
-access = None
 
 # ./main.py
 app = Flask(__name__)
@@ -19,34 +12,27 @@ client = smartcar.AuthClient(
     client_id=os.environ.get('CLIENT_ID'),
     client_secret=os.environ.get('CLIENT_SECRET'),
     redirect_uri=os.environ.get('REDIRECT_URI'),
-    scope=['read_vehicle_info'],
-    test_mode=True
+    scope=[
+        'read_vehicle_info',
+        'read_location',
+        'read_odometer',
+        'control_security',
+        'read_vin',
+      ],
+    test_mode=True,
 )
-
-
-#home page
-@app.route('/')
-@app.route('/index')
-def home():
-    return render_template('index.html')
-  
-
 '''@app.route('usersApp')
 def mainPage():
     return render_template()'''
-def logOut()
+'''def logOut():'''
 
-
-#Authorization Step 1b: Launch Smartcar authorization dialog
+@app.route('/')
 @app.route('/login', methods=['GET'])
 def login():
+    # TODO: Authorization Step 1b: Launch Smartcar authentication dialog
     auth_url = client.get_auth_url()
     return redirect(auth_url)
 
-
-#Authorization Step 3: Handle Smartcar response
-#Request Step 1: Obtain an access token
-#
 @app.route('/exchange', methods=['GET'])
 def exchange():
     code = request.args.get('code')
@@ -58,11 +44,6 @@ def exchange():
     access = client.exchange_code(code)
     return 'Hello', 200
 
-
-
-#Request Step 2: Get vehicle ids
-#Request Step 3: Create a vehicle
-#Request Step 4: Make a request to Smartcar API
 @app.route('/vehicle', methods=['GET'])
 def vehicle():
     # access our global variable to retrieve our access tokens
@@ -75,19 +56,62 @@ def vehicle():
     vehicle = smartcar.Vehicle(vehicle_ids[0], access['access_token'])
 
     info = vehicle.info()
+    #coordinates of current location
+    coordinates = vehicle.location()
+    lat = coordinates['data']['latitude']
+    lon = coordinates['data']['longitude']
+
+    #odometer on kilometers
+    mileage = vehicle.odometer()
+    oilChangemiles = mileage['data']['distance'] + 3000
+    miles_UntilOil_Change = 0.621*(oilChangemiles - mileage['data']['distance'])
+    mileageOriginal = mileage['data']['distance']*0.621
+    
+
+    #dictionary with stats from car
+    stats = {
+        "id":info['id'],
+        "Make":info['make'],
+        "Model":info['model'],
+        "Year":info['year'],
+        "Mileage":mileageOriginal,
+        "OilMiles": miles_UntilOil_Change,
+        "latitude":lat,
+        "longitude":lon,
+    }
+
+
     print(info)
 
-    return jsonify(info)
-  
+    return 'succesfully completed'
+
+'''
 @app.route('/userApp', methods=['GET'])
 def userApp():
     global access
-    access['access_token'])['vehicles']
+    vehicle_ids = smartcar.get_vehicle_ids(
+        access['access_token'])['vehicles']
+
+    # instantiate the first vehicle in the vehicle id list
     vehicle = smartcar.Vehicle(vehicle_ids[0], access['access_token'])
 
+    coordinates = vehicle.location()
+    mileage = vehicle.odometer()
+    oilChangemiles = mileage + 1560
+    milesUntilOil_Change = oilChangemiles - mileage
+    
+    isLocked = vehicle.lock()
+    
+    if isLocked == 'success':
+        isLocked = 'Locked'
+    else:
+        isLocked = 'Unlocked'
 
+    stats = {
+        "OilMiles": milesUntilOil_Change,
+        "lockStatus":isLocked,
+    }
+    return 'howdy',200'''
 
 if __name__ == '__main__':
     app.run(port=8000)
-
-
